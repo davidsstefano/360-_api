1b465fe4f6279cb91e0b5764243b7e4e75c92001
Vinicius Canuto <caanutto@gmail.com>
2021-11-14 11:29:56 -0300

    Algumas atualizações ainda sem integração.

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+/*!
+* Start Bootstrap - Agency v7.0.6 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+let deferredPrompt;
+const addBtn = document.querySelector('#add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+  
+    addBtn.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
+  });
--- public/cadastro_motorista.php
+++ public/cadastro_motorista.php
@@ -1,73 +1,250 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_BR">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/estilo_formulario.css">
+    
+    
+    <!-- <link rel="stylesheet" href="../css/estilo_formulario.css"> -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="../js/app_formulario.js"></script>
-    <title>Document</title>
+   
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Cadastro Motorista</title>
+  <!-- Favicon-->
+  <link rel="icon" type="image/x-icon" href="assets/img/logos/360-degrees-svgrepo-com.svg" />
+
+  <link rel="stylesheet" href="/css/formulario.css">
+  <style>
+    :root {
+      --succes-color: #2ecc71;
+      ;
+      --error-color: #e74c3c;
+    }
+
+    .container {
+      background-color: #fff;
+      border-radius: 5px;
+      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+      width: 400px;
+      margin: 10px auto;
+    }
+
+    h2 {
+      text-align: center;
+      margin: 0 0 20px;
+    }
+
+    .form {
+      padding: 30px 40px;
+    }
+
+    .form-control {
+      margin-bottom: 10px;
+      padding-bottom: 20px;
+      position: relative;
+    }
+
+    .form-control label {
+      color: #777;
+      display: block;
+      margin-bottom: 5px;
+    }
+
+    .form-control input {
+      border: 2px solid #f0f0f0;
+      border-radius: 4px;
+      display: block;
+      width: 100%;
+      padding: 10px;
+      font-size: 14px;
+    }
+
+    .form-control input:focus {
+      outline: 0;
+      border-color: #777;
+
+    }
+
+    .form-control.success input {
+      border-color: var(--succes-color);
+    }
+
+    .form-control.error input {
+      border-color: var(--error-color);
+    }
+
+    .form-control small {
+      color: var(--error-color);
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      visibility: hidden;
+    }
+
+    .form-control.error small {
+      visibility: visible;
+    }
+
+    .form button {
+      cursor: pointer;
+      background-color: #3498db;
+      border: 2px solid #3498db;
+      border-radius: 4px;
+      color: #fff;
+      display: block;
+      padding: 10px;
+      font-size: 16px;
+      margin-top: 20px;
+      width: 100%;
+    }
+  </style>
 </head>
 
 <body>
 
-    <main>
-        <div class="container_cm">
-            <img id="img_logo" src="../img/logo_site_360.jpg" alt="Logo">
-            <div class="form-box">
-
-                <form action="../api/api_cadastromotorista.php" method="POST" enctype="multipart/form-data">
-                    <div>
-                        <h2>Cadastro</h2>
-                    </div>
-                    <div>
-                        <label for="motorista">Digite o Nome: </label>
-                        <input type="text" id="motorista" name="motorista" placeholder="Nome" maxlength="30" required class="form-input">
-                    </div>
-                    <div>
-                        <label for="Carteira">Digite o numero da carteira: </label>
-                        <input type="text" id="carteira" name="carteira" placeholder="Numero carteira" maxlength="11" required class="form-input">
-                    </div>
-                    <div>
-                        <label for="Categoria">Digite a Categoria: </label>
-                        <input type="text" id="categoria" name="categoria" placeholder="Categoria da carteira" maxlength="1" required class="form-input">
-                    </div>
-                    <div>
-                        <label for="data_validade">Digite a data de validade: </label>
-                        <input type="date" id="data_validade" name="data_validade" placeholder="data de validade" required class="form-input">
-                    </div>
-                    <div>
-                        <label for="email_m">Digite o email: </label>
-                        <input type="text" id="email" name="email" placeholder="Email" maxlength="40" required class="form-input">
-                    </div>
-                    <div>
-                        <label for="telefone">Digite o telefone: </label>
-                        <input type="text" id="telefone" name="telefone" placeholder="Telefone" maxlength="15" required class="form-input">
-                    </div>
-                    <div>
-                        <label for="foto">Insira sua o foto: </label>
-                        <input type="file" name="foto_m" placeholder="Foto" maxlength="45" required class="form-input">
-                    </div>
-                    <label for="senha">Digite o senha: </label>
-                    <input type="password" id="senha" name="senha" placeholder="Senha" maxlength="32" required class="form-input">
-
-                    <div>
-                        <label for="confirm_senha">confirme a senha: </label>
-                        <input type="password" id="confirm_senha" name="confirm_senha" placeholder="confirme a senha" maxlength="32" required class="form-input" onkeyup='check();'>
-                    </div>
-                    <span id='message'></span><br>
-                    <div>
-                        <input type="submit" nome="cadastrar" value="cadastrar" class="form-btn">
-                    </div>
-                    <a href="logout.php">SAIR</a>
-                </form>
-            </div>
+  <main>
+  <img id="img_logo" src="/img/logo_site_360.jpg">
+    <div class="container">
+      <form id="form" class="form">
+        <h2>Cadastro</h2>
+        <div class="form-control">
+          <label for="text">Nome</label>
+          <input type="text" id="nome" placeholder="Digite seu nome">
+          <small>Error Message</small>
         </div>
-    </main>
+        <div class="form-control">
+          <label for="text2">Sobrenome</label>
+          <input type="text" id="sobrenome" placeholder="Digite seu sobrenome">
+          <small>Error Message</small>
+        </div>
+        <div class="form-control">
+          <label for="number">CNH</label>
+          <input type="number" id="cnh" placeholder="Digite o número de registro de sua CNH">
+          <small>Error Message</small>
+        </div>
+        <div class="form-control">
+          <label for="email">Categoria</label>
+          <input type="text" id="cnh2" placeholder="Categoria da habilitação">
+          <small>Error Message</small>
+        </div>
+        <div class="form-control">
+          <label for="email">Validade</label>
+          <input type="date" id="cnh3" placeholder="Validade da CNH">
+          <small>Error Message</small>
+        </div>
+        <div class="form-control">
+          <label for="email">Telefone</label>
+          <input type="tel" id="tel" placeholder="Digite seu número">
+          <small>Error Message</small>
+        </div>
+        <div class="form-control">
+          <label for="email">Envie sua foto</label>
+          <input type="file" id="foto">
+          <small>Error Message</small>
+        </div>
+        <div class="form-control">
+          <label for="email">Email</label>
+          <input type="email" id="email" placeholder="Digite seu email">
+          <small>Error Message</small>
+        </div>
+        <div class="form-control">
+          <label for="password">Senha</label>
+          <input type="password" id="password" placeholder="Digite sua senha">
+          <small>Error Message</small>
+        </div>
+        <div class="form-control">
+          <label for="password2">Confirme sua senha</label>
+          <input type="password" id="password2" placeholder="Digite sua senha">
+          <small>Error Message</small>
+        </div>
+        <button>Enviar</button>
 
+      </form>
+    </div>
+  </main>
+
+  <footer class="credit">Copyright &copy; Bite Treechnology 2021</footer>
+  <script>
+    const form = document.getElementById('form');
+    const username = document.getElementById('text' + 'text2');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const password2 = document.getElementById('password2');
+
+    //Show input error messages
+    function showError(input, message) {
+      const formControl = input.parentElement;
+      formControl.className = 'form-control error';
+      const small = formControl.querySelector('small');
+      small.innerText = message;
+    }
+
+    //show success colour
+    function showSucces(input) {
+      const formControl = input.parentElement;
+      formControl.className = 'form-control success';
+    }
+
+    //check email is valid
+    function checkEmail(input) {
+      const re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (re.test(input.value.trim())) {
+        showSucces(input)
+      } else {
+        showError(input, 'Email is not invalid');
+      }
+    }
+
+
+    //checkRequired fields
+    function checkRequired(inputArr) {
+      inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+          showError(input, `${getFieldName(input)} is required`)
+        } else {
+          showSucces(input);
+        }
+      });
+    }
+
+
+    //check input Length
+    function checkLength(input, min, max) {
+      if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+      } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be les than ${max} characters`);
+      } else {
+        showSucces(input);
+      }
+    }
+
+    //get FieldName
+    function getFieldName(input) {
+      return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    }
+
+    // check passwords match
+    function checkPasswordMatch(input1, input2) {
+      if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+      }
+    }
+
+
+    //Event Listeners
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      checkRequired([name, email, password, password2]);
+      checkLength(username, 3, 15);
+      checkLength(password, 6, 25);
+      checkEmail(email);
+      checkPasswordMatch(password, password2);
+    });
+  </script>
 </body>
 
 </html>
